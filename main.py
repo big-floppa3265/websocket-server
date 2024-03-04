5ce4a059c3b7c77f92bda6ffd6beb2cbee9b0899
MichaAI <misha-8-filatov@yandex.ru>
2024-03-04 13:46:40 +0300

    docker container auto build

--- main.py
+++ main.py
@@ -7,22 +7,25 @@ from websockets.server import serve
 
 players = {}
 
+
 async def handler(websocket):
-    print('New player connecting...')
-    await websocket.send('hello!')
+    print("New player connecting...")
+    await websocket.send("hello!")
 
     name = await websocket.recv()
 
-    print(f'{name} just joined!')
+    print(f"{name} just joined!")
 
-    players[name] = {'x': 0, 'y': 0}
-    
+    players[name] = {"x": 0, "y": 0}
+
     async for message in websocket:
         print(message)
-        await websocket.send('a')
+        await websocket.send("a")
+
 
 async def main():
     async with serve(handler, "localhost", 7000):
         await asyncio.Future()  # run forever
+
 
 asyncio.run(main())
